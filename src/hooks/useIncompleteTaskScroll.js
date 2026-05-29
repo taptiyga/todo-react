aaa4ee9705faff4a074290e3971b7a3408614a35
Taptiyga <taptiyga83@mail.ru>
2026-05-29 12:56:55 +0400

    сделал пользовательские хуки

---
+++ src/hooks/useIncompleteTaskScroll.js
@@ -0,0 +1,12 @@
+import { useRef } from "react";
+
+const useIncompleteTaskScroll = (tasks) => {
+  const firstIncompleteTaskRef = useRef(null);
+  const firstIncompleteTaskId = tasks.find(({ isDone }) => !isDone)?.id;
+    return {
+    firstIncompleteTaskRef,
+    firstIncompleteTaskId,  
+    }
+}
+
+export default useIncompleteTaskScroll;
